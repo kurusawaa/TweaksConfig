8242eb4352fc56f357c16dfcdc6fcc385189f975
kurusawaa <130026393+kurusawaa@users.noreply.github.com>
2023-05-04 04:39:18 +0800

    Create configtweaks.py

---
+++ configtweaks.py
@@ -0,0 +1,177 @@
+import os
+
+import time
+
+import sys
+
+#This tool made By Eugene tsu
+
+#Give me credits if You change it thanks for using..
+
+sys.path.insert(1,'0://andriod/data/com.termux.andriodtweak/')
+
+os.system('clear')
+
+print("""\033[91m
+
+░
+
+╔══════════════════════════════════════════╗
+
+║                                          ║
+
+║─█▀▀█ ░█▄─░█ ░█▀▀▄ ░█▀▀█ ▀█▀ ░█▀▀▀█ ░█▀▀▄ ║
+
+║░█▄▄█ ░█░█░█ ░█─░█ ░█▄▄▀ ░█─ ░█──░█ ░█─░█ ║
+
+║░█─░█ ░█──▀█ ░█▄▄▀ ░█─░█ ▄█▄ ░█▄▄▄█ ░█▄▄▀ ║
+
+║                                          ║
+
+║▀▀█▀▀ ░█──░█ ░█▀▀▀ ─█▀▀█ ░█─▄▀            ║
+
+║─░█── ░█░█░█ ░█▀▀▀ ░█▄▄█ ░█▀▄─            ║
+
+║─░█── ░█▄▀▄█ ░█▄▄▄ ░█─░█ ░█─░█            ║
+
+╚══════════════════════════════════════════╝
+
+\033[0m[\033[91m*\033[91m] \033[0mMODEFIDE BY \033[91mEUGENE TSU""")
+
+print()
+
+print()
+
+print()
+
+print("""┏━━━━━━━━━━━━━━━━━━━━━━━━┓
+
+┃     \033[91m[\033[92m1\033[91m] FPS OPTION     ┃
+
+┗━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+
+┃     \033[91m[\033[92m2\033[91m] SPEED INTERNET     ┃
+
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+┏━━━━━━━━━━━━━━━━━━┓
+
+┃     \033[91m[\033[92m3\033[91m] EXIT     ┃
+
+┗━━━━━━━━━━━━━━━━━━┛""")
+
+print()
+
+print()
+
+print()
+
+option = int(input("\033[0m[\33[91m*\033[m] Please Choose ➤ \033[92m"))
+
+if option == 1:
+
+   print("""
+
+╭────────────────╮
+
+│     120 hz     │
+
+╰────────────────╯
+
+╭───────────────╮
+
+│     90 hz     │
+
+╰───────────────╯
+
+""")
+
+   c = int(input("\033[0m[\33[91m*\033[m] Please  Select ➤ \033[91m"))
+
+   time.sleep(1)
+
+   d = input("\033[0m[\33[91m*\033[m] Enter Your Game ➤ \033[91m")
+
+   if d == 120:
+
+     print("\033[0m[\33[91m*\033[m] CONNECTING TO ➤ " + d + " " + "SET TO ➤", c)
+
+   else:
+
+     print("THIS TOOLS FOR MOBILE LEGENDS ONLY")
+
+   if c == 90:
+
+     print()
+
+     
+
+   if d == 'Mobile legends':
+
+    time.sleep(2)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA FPS SMOOTH ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA FPS STABLE ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA PERFORMANCE CONFIG ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA BATTLEFIELD OVERVIEW EXTRA ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA PERMODECONFIG ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA SMOOTH_UI ")
+
+    time.sleep(4)
+
+    print("\033[0m[\33[91m✓\033[m] DONE  ")
+
+   if c == 'Mobile legends':
+
+    time.sleep(2)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA FPS SMOOTH ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA FPS STABLE ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA PERFORMANCE CONFIG ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA BATTLEFIELD OVERVIEW EXTRA ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA PERMODECONFIG ")
+
+    time.sleep(1)
+
+    print("\033[0m[\33[91m*\033[m] INSTALLING ➤ \033[92mULTRA SMOOTH_UI ")
+
+    time.sleep(4)
+
+    print("\033[0m[\33[91m✓\033[m] DONE  ")
+
+if option == 3:
+
+   exit()
+
+if option == 2:
+
+   print("COMING SOON..")
